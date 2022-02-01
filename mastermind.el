86741d179023c67073537b0ab0b8b8b5a8c12dbe
Alex Santoro <santoro63@yahoo.com>
2022-01-31 16:49:12 -0800

    Renamed code to have package prefix 'mm/'

--- mastermind.el
+++ mastermind.el
@@ -2,55 +2,56 @@
 (require 'cl)
 
 ;; some constants
-(setq color-pegs '("R" "G" "Y" "B" "W" "P"))
-(setq puzzle-size 4)
+(setq mm/color-pegs '("R" "G" "Y" "B" "W" "P"))
+(setq mm/puzzle-size 4)
       
-(defun zip (ar1 ar2)
+(defun mm/zip (ar1 ar2)
   (mapcar* #'cons ar1 ar2))
 
-(defun cons-match (c)
+(defun mm/cons-match (c)
   "Return true if the two parts of the cons cell C are equal."
   (equal (car c) (cdr c)))
 
-(defun generate-list (size candidates)
+(defun mm/generate-list (size candidates)
   "Generate a list of size SIZE from elements in CANDIDATES."
   (if (equal 0 size) '()
-    (cons (nth (random 6) candidates) (generate-list (- size 1) candidates))))
+    (cons (nth (random 6) candidates) (mm/generate-list (- size 1) candidates))))
 
-(defun count-black (guess solution)
+(defun mm/count-black (guess solution)
   "Return a count of how many pieces in the guess match the solution in color and notation."
   (let (
-	(matched (-filter #'cons-match (zip guess solution))))
+	(matched (-filter #'mm/cons-match (mm/zip guess solution))))
     (length matched)))
   
-(defun count-white (guess solution)
-  (let ( (unmatched (-filter (lambda (x) (not (cons-match x))) (zip guess solution))) )
-    (count-xx (-map #'car unmatched) (-map #'cdr unmatched))
+(defun mm/count-white (guess solution)
+  (let ( (unmatched (-filter (lambda (x) (not (mm/cons-match x))) (mm/zip guess solution))) )
+    (mm/count-xx (-map #'car unmatched) (-map #'cdr unmatched))
     ))
 
-(defun count-xx (l1 l2)
+(defun mm/count-xx (l1 l2)
+  "Helper counting function for counting whites."
   (cond ( (not l1) 0)
-	( (member (car l1) l2) (+ 1 (count-xx (cdr l1) (remove (car l1) l2))))
-	( t (count-xx (cdr l1) l2))))
+	( (member (car l1) l2) (+ 1 (mm/count-xx (cdr l1) (remove (car l1) l2))))
+	( t (mm/count-xx (cdr l1) l2))))
 
 
-(defun play-round (solution)
+(defun mm/play-round (solution)
   "Play one round of the game given SOLUTION and returns t if the puzzle has been solved."
   (let* ( (guess
 	   (read-string ": "))
-	  (b    (count-black guess solution))
-	  (w    (count-white guess solution))
+	  (b    (mm/count-black guess solution))
+	  (w    (mm/count-white guess solution))
 	  )
     (insert guess " .... " (number-to-string b) "." (number-to-string w) "\n")
     (= 4 b)))
 
 
-(defun play-game ()
-  "Play a game of Mastermind"
-  (let ( (solution (mapconcat #'identity (generate-list puzzle-size color-pegs) ""))
+(defun mm/play-game ()
+  "Play a game of Mastermind."
+  (let ( (solution (mapconcat #'identity (mm/generate-list mm/puzzle-size mm/color-pegs) ""))
 	 (count 1))
     (insert "\nSolution: " solution "\n\n")
-    (while (not (play-round solution))
+    (while (not (mm/play-round solution))
       (setq count (+ count 1)))
     (insert "Found a solution with " (number-to-string count) " tries.\n")))
 
